eef3831e95277186280e32a284e183c93884cd0a
Nick <nicholasmhanco1k@ufl.edu>
2016-12-04 16:44:49 -0500

    Preliminary support for cicgs Changed the arguemnt type to icg to icg_tuple. CICG then takes an array of such. The code looks correct but I've yet to test it.

--- find.c
+++ find.c
@@ -17,42 +17,36 @@ static ubig seq_len(ubig size, ubig seq[size])
 	return size;
 }
 
-ubig icg_period(big modulus, big mul, big add, big seed)
+ubig icg_period(struct icg_tuple t)
 {
-	ubig *seq = icg(modulus, mul, add, seed);
-	ubig len = seq_len(modulus, seq);
+	ubig *seq = icg(t);
+	ubig len = seq_len(t.mod, seq);
 	free(seq);
 	return len;
 }
 
-big rand_odd(big modu)
+big rand_odd(big max)
 {
-    big x;
-    do  {
-	x = rand();
-    } while (x > modu || x < 0 || x % 2);
-    return x;
+	big x;
+	do  {
+		x = rand();
+	} while (x > max || x < 0 || x % 2);
+	return x;
 }
 
 void find(big modu)
 {
-#if 0
-	FILE* fp;
-	fp = fopen("search.txt", 'w');
-	fprintf(fp, "");
-	fclose(fp);
-#endif
+	time_t tm;
+	srand((unsigned) time(&tm));
 
-	time_t t;
-	srand((unsigned) time(&t));
-
-	big mult, addr, seed;
+	struct icg_tuple t;
+	t.mod = modu;
 	for (int i = 0; i < 1000; i++) {
-		mult = rand_odd(modu);
-		addr = rand_odd(modu);
-		seed = rand_odd(modu);
-		printf("%lld\t%lld\t%lld\t%lld\t%llu\n", modu, mult, addr, seed,
-				icg_period(modu, mult, addr, seed));
+		t.mul = rand_odd(modu);
+		t.add = rand_odd(modu);
+		t.seed = rand_odd(modu);
+		printf("%lld\t%lld\t%lld\t%lld\t%llu\n",
+			t.mod, t.mul, t.add, t.seed, icg_period(t));
 	}
 }
 
--- icg.c
+++ icg.c
@@ -35,15 +35,32 @@ static void gen_invs(big modulus, ubig *invs)
 	}
 }
 
-static void gen_seq(big mod, big mul, big add, big seed, ubig *invs, ubig *arr)
+static void gen_seq(struct icg_tuple t, ubig *invs, ubig *seq)
 {
-	big value = seed;
-	for (size_t i = 0; i < (size_t)mod; ++i) {
-		arr[i] = value;
+	big value = t.seed;
+	for (size_t i = 0; i < (size_t)t.mod; ++i) {
+		seq[i] = value;
 		if (invs[value]) {
-			value = (mul * invs[value] + add) % mod;
+			value = (t.mul * invs[value] + t.add) % t.mod;
 		} else {
-			value = add;
+			value = t.add;
+		}
+	}
+}
+
+/* Assumes all icgs use the same modulus. */
+static void gen_cicg_seq(struct icg_tuple *av, size_t avlen, ubig *inv, ubig *s)
+{
+	assert(av != NULL);
+	big v = av[0].seed;
+	for (size_t i = 0; i < (size_t)av[0].mod; ++i) {
+		for (size_t j = 0; j < avlen; ++j) {
+			s[i * avlen + j] = v;
+			if (inv[v]) {
+				v = (av[j].mul* inv[v] + av[j].add) % av[j].mod;
+			} else {
+				v = av[j].add;
+			}
 		}
 	}
 }
@@ -57,18 +74,40 @@ static void print_arr(big size, ubig arr[size])
 	printf("\n");
 }
 
-ubig *icg(big modulus, big mul, big add, big seed)
+ubig *icg(struct icg_tuple args)
 {
-	ubig *invs = malloc(sizeof(ubig) * modulus);
-	ubig *seq = malloc(sizeof(ubig) * modulus);
+	ubig *invs = malloc(sizeof(ubig) * args.mod);
+	ubig *seq = malloc(sizeof(ubig) * args.mod);
 
-	assert(mul >= 0 && add >= 0 && seed >= 0);
-	mul %= modulus;
-	add %= modulus;
-	seed %= modulus;
+	assert(args.mul >= 0 && args.add >= 0 && args.seed >= 0);
+	args.mul %= args.mod;
+	args.add %= args.mod;
+	args.seed %= args.mod;
 
-	gen_invs(modulus, invs);
-	gen_seq(modulus, mul, add, seed, invs, seq);
+	gen_invs(args.mod, invs);
+	gen_seq(args, invs, seq);
+	free(invs);
+	return seq;
+}
+
+/* Assumes all icgs use the same modulus. */
+ubig *cicg(struct icg_tuple *argvec, size_t avcnt)
+{
+	assert(argvec != NULL);
+	ubig *invs = malloc(sizeof(*invs) * argvec[0].mod);
+	ubig *seq = malloc(sizeof(*seq) * argvec[0].mod * avcnt);
+
+	for (size_t i = 0; i < avcnt; ++i) {
+		argvec[i].mul %= argvec[i].mod;
+		argvec[i].add %= argvec[i].mod;
+		argvec[i].seed %= argvec[i].mod;
+
+		struct icg_tuple a = argvec[i];
+		assert(a.mul >= 0 && a.add >= 0 && a.seed >= 0);
+	}
+
+	gen_invs(argvec[0].mod, invs);
+	gen_cicg_seq(argvec, avcnt, invs, seq);
 	free(invs);
 	return seq;
 }
